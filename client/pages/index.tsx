76a0d32969d90596edef712121f31379d163a29c
asce-del <vlad.yaromiy@gmail.com>
2021-03-19 12:38:02 +0200

    typeScripted

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+interface homeProps {}
+
+const Home: React.FC<homeProps> = ({}) => {
+  return (
+    <div>
+      Welcome to FloppaHouse
+    </div>
+  )
+}
+
+export default Home
---
+++ client/pages/login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface loginProps {}
+
+const Login: React.FC<loginProps> = ({}) => {
+  return <div>login</div>;
+};
+
+
+export default Login
