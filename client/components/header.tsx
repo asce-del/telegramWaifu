278532030dc3269cdf6e01c2a41ec6b6200848f6
asce-del <vlad.yaromiy@gmail.com>
2021-04-07 19:22:47 +0300

    styles some

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Main } from "./main";
 interface headerProps {}
 
 export const Header: React.FC<headerProps> = ({}) => {
@@ -11,11 +12,12 @@ export const Header: React.FC<headerProps> = ({}) => {
             className="relative flex items-center justify-between sm:h-10 lg:justify-start"
             aria-label="Global"
           >
+            <h1 className="hidden md:flex p-5 font-bold text-2xl text-indigo-600 cursor-pointer hover:text-indigo-500">Licode.io</h1>
             <div className="hidden md:flex w-full md:space-x-8">
               <div className="w-4/5">
                 <a
                   href="#"
-                  className="p-5 font-medium text-gray-500 hover:text-gray-900"
+                  className="p-5 font-medium text-gray-500  hover:text-gray-900"
                 >
                   Product
                 </a>
@@ -57,11 +59,7 @@ export const Header: React.FC<headerProps> = ({}) => {
           <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
             <div className="px-5 flex items-center justify-between">
               <div>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                  alt=""
-                />
+              <h1 className="p-5 font-bold text-2xl text-indigo-600 cursor-pointer hover:text-indigo-500">Licode.io</h1>
               </div>
               <div className="-mr-2">
                 <button
@@ -148,39 +146,7 @@ export const Header: React.FC<headerProps> = ({}) => {
           </div>
         </div>
 
-        <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-          <div className="sm:text-center lg:text-left">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block xl:inline">Data to enrich your</span>
-              <span className="block text-indigo-600 xl:inline">
-                online business
-              </span>
-            </h1>
-            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
-            </p>
-            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-              <div className="rounded-md shadow">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-                >
-                  Get started
-                </a>
-              </div>
-              <div className="mt-3 sm:mt-0 sm:ml-3">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-                >
-                  Live demo
-                </a>
-              </div>
-            </div>
-          </div>
-        </main>
+       <Main />
       </div>
     </div>
   );
